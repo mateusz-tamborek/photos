8bb69fe01b926e6a9c3b2601baab4dce7e863499
Mateusz Tamborek <mateusz.tamborek@gmail.com>
2019-07-16 00:32:01 +0200

    Added Swagger for convenience

---
+++ src/main/java/pl/com/tambo/photos/config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package pl.com.tambo.photos.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+}
